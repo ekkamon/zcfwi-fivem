60d4d9281f5eeef28dc4ac6282a142611cbc5b7a
Z4C0N <34152283+ekkamon@users.noreply.github.com>
2022-05-10 11:24:13 +0700

    Delete import file

--- client/player.d.ts
+++ client/player.d.ts
@@ -1,4 +1,13 @@
-import { Item } from '../shared/index';
+export declare interface Item {
+  name: string;
+  count: number;
+  type: string;
+  rare: number;
+  canUsable: boolean;
+  canGive: boolean;
+  canRemove: boolean;
+  canHotkey: boolean;
+}
 
 interface Job {
   name: string;
